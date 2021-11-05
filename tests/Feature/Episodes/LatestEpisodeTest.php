af74d8b689457de2609c8dbc6a3f05d21e678b31
Matteo Mangoni <matt.mangoni@gmail.com>
2021-11-05 17:33:33 +0100

    chore: minor LatestEpisodeTest refactor

--- tests/Feature/Episodes/LatestEpisodeTest.php
+++ tests/Feature/Episodes/LatestEpisodeTest.php
@@ -26,7 +26,7 @@ it('shows the latest episode page to newsers', function () {
 
     get('/latest')->assertStatus(200)->assertInertia(fn (Assert $page) => $page
         ->component('Episodes/LatestEpisode')
-        ->has('episode.id')
+        ->hasAll(['episode.id', 'episode.news'])
         ->where('episode.id', $episode->id)
     );
 });
