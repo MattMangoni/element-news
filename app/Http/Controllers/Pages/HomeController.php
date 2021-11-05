2801e43f860ec159678047c3cd58ba34c1df5c11
Matteo Mangoni <matt.mangoni@gmail.com>
2021-11-05 11:18:58 +0100

    HomePage and file compilation

---
+++ app/Http/Controllers/Pages/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Response;
+
+class HomeController extends Controller
+{
+    public function __invoke(Request $request): Response
+    {
+        return inertia('HomePage');
+    }
+}
