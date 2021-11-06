8a109a07eef4728111a6219b0c585f4c5b6f5699
Matteo Mangoni <matt.mangoni@gmail.com>
2021-11-06 16:23:13 +0100

    fix: route order change

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,13 @@ Route::middleware(['auth', 'newser'])->group(function () {
     Route::get('/latest', LatestEpisodeController::class)->name('latest-episode');
 
     Route::prefix('/news')->as('news.')->group(function () {
-        Route::get('/{news:slug}', ShowNewsController::class)->name('show');
         Route::get('/create', [CreateNewsController::class, 'create'])->name('create');
         Route::post('/create', [CreateNewsController::class, 'store'])->name('store');
         Route::get('/{news:slug}/edit', [EditNewsController::class, 'edit'])->name('edit');
         Route::patch('/{news:slug}', [EditNewsController::class, 'update'])->name('update');
         Route::delete('/{news:slug}', DeleteNewsController::class)->name('delete');
+
+        Route::get('/{news:slug}', ShowNewsController::class)->name('show');
     });
 });
 
