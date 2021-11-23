cb5b45b3e812ae90f9e4d002a8533a520c2bea07
Matteo Mangoni <matt.mangoni@gmail.com>
2021-11-23 16:28:27 +0100

    chore: added back link to create news page

--- resources/js/Pages/News/CreateNews.vue
+++ resources/js/Pages/News/CreateNews.vue
@@ -3,19 +3,23 @@
 
     <div>
         <page-header>
-            Aggiungi una news
+            <div class="flex justify-between items-center">
+                <h1>Aggiungi una news</h1>
+                <Link class="text-sm font-normal" :href="route('latest-episode')">Torna alla pagina iniziale</Link>
+            </div>
         </page-header>
         <page-content>
-            <form @submit.prevent="form.post('/login')"></form>
+            <form @submit.prevent="form.post('/login')">
+                // TODO: create form
+            </form>
         </page-content>
     </div>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/inertia-vue3'
 import { Head } from '@inertiajs/inertia-vue3'
 import { useForm } from '@inertiajs/inertia-vue3'
 import PageHeader from "@/Layouts/Shared/PageHeader"
 import PageContent from "@/Layouts/Shared/PageContent"
-
-
 </script>
